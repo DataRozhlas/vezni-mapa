24d457b9c803221522d69de2fec5708842bb6d11
timichal <michal@zlatkovsky.cz>
2018-12-28 00:11:17 +0100

    apply targetblank after DOM is loaded

--- js/targetblank.js
+++ js/targetblank.js
@@ -1,8 +1,10 @@
-const links = document.getElementsByTagName("a");
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.getElementsByTagName("a");
 
-Array.prototype.forEach.call(links, (link) => {
-  if (link.hostname !== window.location.hostname) {
-    link.target = "_blank";
-    link.rel = "noopener noreferrer";
-  }
+  Array.prototype.forEach.call(links, (link) => {
+    if (link.hostname !== window.location.hostname) {
+      link.target = "_blank";
+      link.rel = "noopener noreferrer";
+    }
+  });
 });
